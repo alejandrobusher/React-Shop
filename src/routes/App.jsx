4dce8b3abc865c0c60f1e5dba97a00ccb5d8f35c
alejandrobusher <alebusher32@hotmail.com>
2023-02-14 10:43:21 -0500

    Router DOM added

---
+++ src/routes/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import Layout from "../containers/Layout"
+import Login from "../containers/Login"
+import RecoveryPassword from '../containers/RecoveryPassword'
+
+import "../styles/global.css"
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Layout>
+                    <Router exact path="/" component={Home} />
+                    <Router exact path="/login" component={Login} />
+                    <Router exact path="/recovery-password" component={RecoveryPassword} />
+                    <Router component={NotFound} />
+                </Layout>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default App;
